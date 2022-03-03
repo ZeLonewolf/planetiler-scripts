e043a963a1789a06042253bad9179427fff20cdd
Brian Sperlongano <zelonewolf@gmail.com>
2022-03-03 03:10:34 +0000

    Setup 512G scripts

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # planetiler-scripts
 Scripts for building planetiler
+
+One-time planet rebuild scripts:
+
+* AWS image with at least 512G ram:
+  `wget -O - https://raw.githubusercontent.com/ZeLonewolf/planetiler-scripts/main/aws.512GB.sh`
---
+++ aws.512GB.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Executed from home directory of default AWS image user
+# Requires machine with 512GB ram.  All disk operations happen in ramdisk until final copy to EFS share.
+
+sudo apt -y install nfs-common git
+sudo mkdir /mnt/efs
+
+# Mount EFS share where mbtiles will be stored
+# fs ID needs to change if using different EFS mount
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-04aa7452c3c7a4ac2.efs.us-east-2.amazonaws.com:/ /mnt/efs
+
+git clone https://github.com/ZeLonewolf/planetiler-scripts.git
+
+planetiler-scripts/00_setup.sh
+
+planetiler-scripts/01_mount_ramdisk400.sh
+
+cd build
+
+planetiler-scripts/02_download_planet.sh
+planetiler-scripts/03_render_planet.sh
+planetiler-scripts/04_copy_mbtiles.sh
