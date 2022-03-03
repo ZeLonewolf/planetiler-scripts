e2ab7f1093c0c5b533f23abadecc8c1ff898ee98
Brian Sperlongano <zelonewolf@gmail.com>
2022-03-03 11:50:27 +0000

    Add results of 3/3/22 build attempt

--- 02_download_planet.sh
+++ 02_download_planet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo docker run --memory=10g -e JAVA_TOOL_OPTIONS='-Xmx10g -Xms10g -XX:OnOutOfMemoryError="kill -9 %p"' -v "$(pwd)/data":/data \
+sudo docker run --memory=100g -e JAVA_TOOL_OPTIONS='-Xmx80g -Xms80g -XX:OnOutOfMemoryError="kill -9 %p"' -v "$(pwd)/data":/data \
   ghcr.io/onthegomap/planetiler:latest --area=planet --bounds=world --download --download-threads=10 --download-chunk-size-mb=1000 \
   --only-download
 
