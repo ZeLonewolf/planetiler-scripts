f848144fdb509949c3503de14225b7b5c5db7cfb
Brian Sperlongano <zelonewolf@gmail.com>
2022-03-03 08:32:26 -0500

    Update 02_download_planet.sh
    Add wikidata fetch

--- 02_download_planet.sh
+++ 02_download_planet.sh
@@ -2,6 +2,8 @@
 
 sudo docker run --memory=100g -e JAVA_TOOL_OPTIONS='-Xmx80g' -v "$(pwd)/data":/data \
   ghcr.io/onthegomap/planetiler:latest --area=planet --bounds=world --download --download-threads=10 --download-chunk-size-mb=1000 \
-  --only-download --fetch-wikidata
+  --only-fetch-wikidata
+
+sudo cp data/sources/wikidata_names.jsom /mnt/efs/
 
 sudo pyosmium-up-to-date -vvvv --size 10000 data/sources/planet.osm.pbf
