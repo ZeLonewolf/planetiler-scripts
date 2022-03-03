74c5a8f254f7b58c4a63118725e06ecb5ea062e0
Brian Sperlongano <zelonewolf@gmail.com>
2022-03-03 08:47:25 -0500

    Update copies

--- 04_copy_mbtiles.sh
+++ 04_copy_mbtiles.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rm -rf /mnt/efs/output.mbtiles
-rsync --progress data/output.mbtiles /mnt/efs/planet.mbtiles
+sudo rm -rf /mnt/efs/output.mbtiles
+sudo mv /mnt/efs/new-planet.mbtiles output.mbtiles
