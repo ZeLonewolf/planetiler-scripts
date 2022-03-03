2f1297861da183d93ae9e3d894da6229b03607ed
Brian Sperlongano <zelonewolf@gmail.com>
2022-03-03 12:01:19 +0000

    Update scripts to avoid crashes

--- 02_download_planet.sh
+++ 02_download_planet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo docker run --memory=100g -e JAVA_TOOL_OPTIONS='-Xmx80g -Xms80g -XX:OnOutOfMemoryError="kill -9 %p"' -v "$(pwd)/data":/data \
+sudo docker run --memory=100g -e JAVA_TOOL_OPTIONS='-Xmx80g' -v "$(pwd)/data":/data \
   ghcr.io/onthegomap/planetiler:latest --area=planet --bounds=world --download --download-threads=10 --download-chunk-size-mb=1000 \
-  --only-download
+  --only-download --fetch-wikidata
 
 sudo pyosmium-up-to-date -vvvv --size 10000 data/sources/planet.osm.pbf
--- 03_render_planet.sh
+++ 03_render_planet.sh
@@ -5,6 +5,6 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 touch logs.txt
 tail -f logs.txt | nc seashells.io 1337 > /tmp/seashells & sleep 10
 
-sudo docker run --memory=105g -e JAVA_TOOL_OPTIONS='-Xmx100g -Xms100g -XX:OnOutOfMemoryError="kill -9 %p"' -v "$(pwd)/data":/data \
-ghcr.io/onthegomap/planetiler:latest --area=planet --bounds=world --download --download-threads=10 --download-chunk-size-mb=1000 \
---nodemap-type=sparsearray --nodemap-storage=ram --building_merge_z13=false --fetch-wikidata | tee -a logs.txt
+sudo docker run --memory=105g -e JAVA_TOOL_OPTIONS='-Xmx100g' -v "$(pwd)/data":/data \
+ghcr.io/onthegomap/planetiler:latest --area=planet --bounds=world \
+--nodemap-type=sparsearray --nodemap-storage=ram --building_merge_z13=false | tee -a logs.txt
