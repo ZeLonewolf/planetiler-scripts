e22557af18218a02a99640c839728c3c3fc77153
Brian Sperlongano <zelonewolf@gmail.com>
2022-03-20 12:37:46 -0400

    Update 02_download_planet_parallel.sh

--- 02_download_planet_parallel.sh
+++ 02_download_planet_parallel.sh
@@ -8,6 +8,7 @@ sudo pyosmium-up-to-date -vvvv --size 10000 data/sources/planet.osm.pbf &
 sudo cp /mnt/efs/wikidata_names.json data/sources/ \
   && sudo docker run -e JAVA_TOOL_OPTIONS='-Xmx10g' -v "$(pwd)/data":/data \
   ghcr.io/onthegomap/planetiler:latest --area=planet --bounds=world --only-fetch-wikidata \
-  && sudo cp data/sources/wikidata_names.json /mnt/efs/ &
+  && sudo cp data/sources/wikidata_names.json /mnt/efs/ \
+  && echo "Wikidata download complete" &
 
 wait
