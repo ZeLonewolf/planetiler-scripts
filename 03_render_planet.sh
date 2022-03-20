8c8e0ac1e67f3ea99322bb453c5c34c5bd5dd126
Brian Sperlongano <zelonewolf@gmail.com>
2022-03-19 21:36:26 -0400

    Update 03_render_planet.sh

--- 03_render_planet.sh
+++ 03_render_planet.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sudo trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 sudo touch logs.txt
-sudo tail -f logs.txt | nc seashells.io 1337 > /tmp/seashells & sleep 10
+sudo tail -f logs.txt | sudo nc seashells.io 1337 > /tmp/seashells & sleep 10
 
 sudo docker run -e JAVA_TOOL_OPTIONS='-Xmx100g' -v "$(pwd)/data":/data -v /mnt/efs:/output \
 ghcr.io/onthegomap/planetiler:latest --area=planet --bounds=world \
   --mbtiles=/output/new-planet.mbtiles \
-  --nodemap-type=sparsearray --nodemap-storage=ram --building_merge_z13=false | tee -a logs.txt
+  --nodemap-type=sparsearray --nodemap-storage=ram --building_merge_z13=false | sudo tee -a logs.txt
