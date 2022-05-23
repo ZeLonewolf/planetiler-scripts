09dea80ddb4b7004865ce9d7554f1574bf24e30c
Brian Sperlongano <zelonewolf@gmail.com>
2022-05-22 20:51:29 -0400

    Update 03_render_planet.sh

--- 03_render_planet.sh
+++ 03_render_planet.sh
@@ -8,6 +8,7 @@ sudo tail -f logs.txt | sudo nc seashells.io 1337 > /tmp/seashells & sleep 10
 sudo docker run -e JAVA_TOOL_OPTIONS='-Xmx115g' -v "$(pwd)/data":/data -v /mnt/efs:/output \
 ghcr.io/onthegomap/planetiler:latest --area=planet --bounds=world \
   --mbtiles=/output/new-planet.mbtiles \
-  --transportation-name-brunnel \
+  --transportation_name_size_for_shield \ 
+  --transportation_name_limit_merge \
   --storage=ram --mmap-temp --nodemap-type=array --building_merge_z13=false | sudo tee -a logs.txt
   
