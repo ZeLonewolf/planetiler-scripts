fd6b75ee029e1720e122621c5a100def4816bcc0
Brian Sperlongano <zelonewolf@gmail.com>
2022-05-22 20:39:04 -0400

    Update 03_render_planet.sh

--- 03_render_planet.sh
+++ 03_render_planet.sh
@@ -5,8 +5,9 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 sudo touch logs.txt
 sudo tail -f logs.txt | sudo nc seashells.io 1337 > /tmp/seashells & sleep 10
 
-sudo docker run -e JAVA_TOOL_OPTIONS='-Xmx100g' -v "$(pwd)/data":/data -v /mnt/efs:/output \
+sudo docker run -e JAVA_TOOL_OPTIONS='-Xmx115g' -v "$(pwd)/data":/data -v /mnt/efs:/output \
 ghcr.io/onthegomap/planetiler:latest --area=planet --bounds=world \
   --mbtiles=/output/new-planet.mbtiles \
-  --storage=ram --mmap-temp --nodemap-type=sparsearray --building_merge_z13=false | sudo tee -a logs.txt
+  --transportation-name-brunnel \
+  --storage=ram --mmap-temp --nodemap-type=array --building_merge_z13=false | sudo tee -a logs.txt
   
