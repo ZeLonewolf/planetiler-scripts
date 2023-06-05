fe3b11f5a621685c8a9c329f32a4b697228b3e04
Brian Sperlongano <zelonewolf@gmail.com>
2023-06-05 12:59:03 -0400

    Update 05_run_tileserver.sh
    Update tileserver command

--- 05_run_tileserver.sh
+++ 05_run_tileserver.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 docker stop $(docker ps -aq)
-docker run -it --restart always -v /mnt/efs/fs1/:/data -p 8080:8080 maptiler/tileserver-gl \
-  --public_url https://6ug7hetxl9.execute-api.us-east-2.amazonaws.com/ --mbtiles planet.mbtiles
+docker run -it --restart always -v /mnt/efs/fs1/:/data -p 8080:8080 maptiler/tileserver-gl
+# Adjust below to serve on a specific URL or specific mbtiles.  Otherwise config.json is used 
+# --public_url https://6ug7hetxl9.execute-api.us-east-2.amazonaws.com/ --mbtiles planet.mbtiles
