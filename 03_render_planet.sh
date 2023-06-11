99cc2098234f64fc2265008e41894464de687e1c
Brian Sperlongano <zelonewolf@gmail.com>
2023-06-10 21:48:25 -0400

    Update 03_render_planet.sh
    update deprecated switch

--- 03_render_planet.sh
+++ 03_render_planet.sh
@@ -9,7 +9,7 @@ run() {
   set -x
   sudo docker run -e JAVA_TOOL_OPTIONS='-Xmx30g' -v "$(pwd)/data":/data \
     ghcr.io/onthegomap/planetiler:latest --area=planet --bounds=world \
-    --mbtiles=/data/new-planet.mbtiles \
+    --output=/data/new-planet.mbtiles \
     --transportation_name_size_for_shield \
     --transportation_name_limit_merge \
     --boundary-osm-only \
