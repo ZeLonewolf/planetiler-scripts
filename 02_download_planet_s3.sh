fa1ab986187833bf6beb6ddda61b55d197523a39
Brian Sperlongano <zelonewolf@gmail.com>
2023-08-05 12:44:14 -0400

    Update for pmtiles

---
+++ 02_download_planet_s3.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir -p data/sources
+
+sudo docker run -e JAVA_TOOL_OPTIONS='-Xmx80g' -v "$(pwd)/data":/data \
+  ghcr.io/onthegomap/planetiler:latest --osm-url=https://planet-pmtiles.s3.us-east-2.amazonaws.com/planet.osd.pbf \
+  --download --download-threads=20 --download-chunk-size-mb=500 --only-fetch-wikidata
