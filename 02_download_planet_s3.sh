9910ea71a9e60654e2165dca5b723363dbe0c751
Brian Sperlongano <zelonewolf@gmail.com>
2023-08-05 17:57:51 -0400

    Update 02_download_planet_s3.sh

--- 02_download_planet_s3.sh
+++ 02_download_planet_s3.sh
@@ -2,7 +2,10 @@
 
 sudo mkdir -p data/sources
 
+sudo docker run -e JAVA_TOOL_OPTIONS='-Xmx80g' -v "$(pwd)/data":/data \
+  ghcr.io/onthegomap/planetiler:latest --download --download-only --only-fetch-wikidata
+
 sudo aws s3 cp s3://planet-pmtiles/planet.osm.pbf data/sources/
 
-sudo docker run -e JAVA_TOOL_OPTIONS='-Xmx80g' -v "$(pwd)/data":/data \
-  ghcr.io/onthegomap/planetiler:latest --download --only-fetch-wikidata
+# Remove incidental download
+sudo rm -f data/sources/monaco.osm.pbf
