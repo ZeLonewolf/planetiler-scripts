b31da3a52be9c0674803a1d679fc0f5b7113300a
Brian Sperlongano <zelonewolf@gmail.com>
2023-08-05 17:13:55 -0400

    Update 02_download_planet_s3.sh

--- 02_download_planet_s3.sh
+++ 02_download_planet_s3.sh
@@ -2,6 +2,7 @@
 
 sudo mkdir -p data/sources
 
+sudo aws s3 cp s3://planet-pmtiles/planet.osm.pbf data/sources/
+
 sudo docker run -e JAVA_TOOL_OPTIONS='-Xmx80g' -v "$(pwd)/data":/data \
-  ghcr.io/onthegomap/planetiler:latest --osm-url=https://planet-pmtiles.s3.us-east-2.amazonaws.com/planet.osd.pbf \
-  --download --download-threads=20 --download-chunk-size-mb=500 --only-fetch-wikidata
+  ghcr.io/onthegomap/planetiler:latest --only-fetch-wikidata
